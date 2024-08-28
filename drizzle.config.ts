880855f6daae0d07d36697847a0ca31569e454f0
Nguyen Duy Long <nduylong9501@gmail.com>
2024-08-28 10:47:37 +0700

    feat: setup migration

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,6 @@
-import { defineConfig } from '@drepkovsky/drizzle-migrations';
-import 'dotenv/config';
-import { drizzle } from 'drizzle-orm/node-postgres';
-import { Pool } from 'pg';
+import { defineConfig } from 'drizzle-kit';
 
-export const drizzleMigration: ReturnType<typeof defineConfig> = defineConfig({
+export default defineConfig({
   dialect: 'postgresql',
   out: './src/database/drizzle',
   schema: './src/entities/*.ts',
@@ -26,44 +23,4 @@ export const drizzleMigration: ReturnType<typeof defineConfig> = defineConfig({
   migrations: {
     schema: 'public',
   },
-  getMigrator: (): any => {
-    const pgConfig = {
-      host: process.env.DATABASE_HOST || 'localhost',
-      port: +process.env.DATABASE_PORT || 5432,
-      user: process.env.DATABASE_USERNAME || 'postgres',
-      password: process.env.DATABASE_PASSWORD || 'password',
-      database: process.env.DATABASE_DB_NAME || 'example',
-      ssl: false,
-    };
-
-    const pool = new Pool(pgConfig);
-    return drizzle(pool);
-  },
 });
-
-export default drizzleMigration;
-
-// export default defineConfig({
-//   dialect: 'postgresql',
-//   out: './src/database/drizzle',
-//   schema: './src/entities/*.ts',
-//   introspect: {
-//     casing: 'camel',
-//   },
-//   dbCredentials: {
-//     host: process.env.DATABASE_HOST || 'localhost',
-//     port: Number(process.env.DATABASE_PORT) || 5432,
-//     user: process.env.DATABASE_USERNAME || 'postgres',
-//     password: process.env.DATABASE_PASSWORD || 'password',
-//     database: process.env.DATABASE_DB_NAME || 'example',
-//     ssl: false,
-//   },
-//   // Print all statements
-//   verbose: true,
-//   breakpoints: true,
-//   // Always ask for confirmation
-//   strict: true,
-//   migrations: {
-//     schema: 'public',
-//   },
-// });
---
+++ drizzle.migration.config.ts
@@ -0,0 +1,69 @@
+import { defineConfig } from '@llong2195/drizzle-migrations';
+import 'dotenv/config';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Pool } from 'pg';
+
+export const drizzleMigration: ReturnType<typeof defineConfig> = defineConfig({
+  dialect: 'postgresql',
+  out: './src/database/drizzle',
+  schema: './src/entities/schema.ts',
+  introspect: {
+    casing: 'camel',
+  },
+  dbCredentials: {
+    host: process.env.DATABASE_HOST || 'localhost',
+    port: Number(process.env.DATABASE_PORT) || 5432,
+    user: process.env.DATABASE_USERNAME || 'postgres',
+    password: process.env.DATABASE_PASSWORD || 'password',
+    database: process.env.DATABASE_DB_NAME || 'example',
+    ssl: false,
+  },
+  // Print all statements
+  verbose: true,
+  breakpoints: true,
+  // Always ask for confirmation
+  strict: true,
+  migrations: {
+    schema: 'public',
+  },
+  getMigrator: (): any => {
+    const pgConfig = {
+      host: process.env.DATABASE_HOST || 'localhost',
+      port: +process.env.DATABASE_PORT || 5432,
+      user: process.env.DATABASE_USERNAME || 'postgres',
+      password: process.env.DATABASE_PASSWORD || 'password',
+      database: process.env.DATABASE_DB_NAME || 'example',
+      ssl: false,
+    };
+
+    const pool = new Pool(pgConfig);
+    return drizzle(pool);
+  },
+});
+
+export default drizzleMigration;
+
+// export default defineConfig({
+//   dialect: 'postgresql',
+//   out: './src/database/drizzle',
+//   schema: './src/entities/*.ts',
+//   introspect: {
+//     casing: 'camel',
+//   },
+//   dbCredentials: {
+//     host: process.env.DATABASE_HOST || 'localhost',
+//     port: Number(process.env.DATABASE_PORT) || 5432,
+//     user: process.env.DATABASE_USERNAME || 'postgres',
+//     password: process.env.DATABASE_PASSWORD || 'password',
+//     database: process.env.DATABASE_DB_NAME || 'example',
+//     ssl: false,
+//   },
+//   // Print all statements
+//   verbose: true,
+//   breakpoints: true,
+//   // Always ask for confirmation
+//   strict: true,
+//   migrations: {
+//     schema: 'public',
+//   },
+// });
